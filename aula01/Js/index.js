466dfb293e05f1654a9cf7887ce1f21454127a3b
Tayane Cristina Alves dos Santos <tay-cristinarj@hotmail.com>
2023-05-19 13:23:28 -0300

    first commit

---
+++ aula01/Js/index.js
@@ -0,0 +1,4 @@
+// exercício - Exiba no seu console a frase a seguir e substitua os valores em aspas pelas suas próprias informações:
+// Meu nome é "nome", Estou aprendendo Javascript as 10h horas da manhã
+
+console.log('Meu nome é "Tayane Cristina". Estou aprendendo Javascript ás' ,13 , 'da tarde')
